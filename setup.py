ce52b73e563dd005c3841d95e3bc0dafcb0237b1
shadow <yanjlee@163.com>
2024-05-30 21:32:29 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
     ],
     include_package_data=True,
-    install_requires=["httpx", "pynacl", "inflect", "slugify"],
+    install_requires=["httpx", "pynacl", "inflect", "pick","path","GuDory2","configparser","pypandoc","python-slugify","vb-console"],
     name="suiutils-py",
     packages=["suiutils_py"],
     url="https://github.com/satisfywithmylife/sui_python_sdk",
